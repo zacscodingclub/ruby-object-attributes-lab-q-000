d0baf6aa9541ddecf3bd3ca9e9ce34ecaf9b2d00
zacscodingclub <zbaston@gmail.com>
2015-11-29 17:57:28 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(name_string)
+    @name = name_string
+  end
+
+  def breed=(breed_string)
+    @breed = breed_string
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(name_string)
+    @name = name_string
+  end
+
+  def job=(job_string)
+    @job = job_string
+  end
+
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
+end
